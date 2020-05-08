d9e1e0542ebaa81ca48608ef4ee7221ca501667f
ciurkoj <jakub1798@gmail.com>
2020-05-08 12:18:20 +0100

    Updates

---
+++ precache-manifest.b1aad04174640414baf3c9aa0f9bcee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9df6b6703debfa998a80be3af7abbf8e",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "0fd14dbc9ae9572729c6",
+    "url": "/my_portfolio/static/css/main.22b73c91.chunk.css"
+  },
+  {
+    "revision": "652ae790720b6d1cbf34",
+    "url": "/my_portfolio/static/js/2.e31af3b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio/static/js/2.e31af3b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fd14dbc9ae9572729c6",
+    "url": "/my_portfolio/static/js/main.258559ed.chunk.js"
+  },
+  {
+    "revision": "93ee410a55207620eddf",
+    "url": "/my_portfolio/static/js/runtime-main.74f6473c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
