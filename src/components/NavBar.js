81d04189a5fc18f6d6a5571ca4011d687af412d3
ciurkoj <jakub1798@gmail.com>
2020-05-11 00:44:47 +0100

    Fixed minor issues with NavBar
    In NavBar.css posistion changed to static
    In NavBar.js "class" changed to "className" due to naming error

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,50 +3,43 @@ import "./NavBar.css";
 
 function NavBar() {
   return (
-    <div id="header-wrap" class="stuck">
-      <label for="nav-trigger" class="nav-trigger hamburger">
-        <span class="open-btn">
-          <span class="mobile"></span>
-          <span class="mobile"></span>
-          <span class="mobile"></span>
-        </span>
-      </label>
+    <div id="header-wrap" className="stuck">
       <div id="logo">
-        <span class="wsite-logo">
+        <span className="wsite-logo">
           <a href="/">
             <span id="wsite-title">CIURKO JAKUB</span>
           </a>
         </span>
       </div>
-      <div id="nav" class="">
-        <div class="container">
-          <ul class="wsite-menu-default">
-            <li id="active" class="wsite-menu-item-wrap   wsite-nav-1">
-              <a href="/" class="wsite-menu-item">
+      <div id="nav" className="">
+        <div className="container">
+          <ul className="wsite-menu-default">
+            <li id="active" className="wsite-menu-item-wrap   wsite-nav-1">
+              <a href="/" className="wsite-menu-item">
                 Home
               </a>
             </li>
             <li
               id="pg706666462202140662"
-              class="wsite-menu-item-wrap   wsite-nav-2"
+              className="wsite-menu-item-wrap   wsite-nav-2"
             >
-              <a href="/blog.html" class="wsite-menu-item">
+              <a href="/blog.html" className="wsite-menu-item">
                 Blog
               </a>
             </li>
             <li
               id="pg383559022164693219"
-              class="wsite-menu-item-wrap   wsite-nav-3"
+              className="wsite-menu-item-wrap   wsite-nav-3"
             >
-              <a href="/portfolio.html" class="wsite-menu-item">
+              <a href="/portfolio.html" className="wsite-menu-item">
                 Portfolio
               </a>
             </li>
             <li
               id="pg240834667266101901"
-              class="wsite-menu-item-wrap   wsite-nav-4"
+              className="wsite-menu-item-wrap   wsite-nav-4"
             >
-              <a href="/contact.html" class="wsite-menu-item">
+              <a href="/contact.html" className="wsite-menu-item">
                 Contact
               </a>
             </li>
