e3e5a5f041aecff2018a5fa5f09abe3d756f442b
ciurkoj <jakub1798@gmail.com>
2020-05-27 11:45:52 +0100

    Fixed description on project's card Removed animation from landing page Project description on each card has the same height It improves website's design by keeping it standardised
    Removed react's logo from landing page
    It is not needed anymore

--- src/components/about.js
+++ src/components/about.js
@@ -11,17 +11,16 @@ class About extends Component {
           <Cell col={12}>
             <h1 style={{ fontSize: "60pt" }}>Hi, I am Jakub </h1>
             <h2> and this is my website.</h2>
-            <p>
+            <h6>
               It is not finished at all. It will take a lot time finish it as I
               imagine it, but{" "}
               <h4>
-                {" "}
                 <i>"done is better than perfect"</i>{" "}
               </h4>{" "}
               and this is why I've decided to publish it. I'm aware of its
               scalabilty issues- I'm working on it. Please be patient
-            </p>
-            <p>
+            </h6>
+            <h6>
               At the moment I'm 2nd year Computer Science Student looking for a
               placemnt in Web Development. There are a few of my projects that I
               would like to show you. I'm still trying to figure out where I
@@ -29,7 +28,7 @@ class About extends Component {
               projects. Most of them I made as my university task, but there are
               some I made on my own. Click
               <Link to="/my_portfolio/projects">HERE</Link> find out more.
-            </p>
+            </h6>
 
             <div className="contact-list"></div>
           </Cell>
