5e6c117f92958ed30a1fbd6b19f55ef7b0e10fe0
ciurkoj <jakub1798@gmail.com>
2020-05-27 19:41:10 +0100

    First successful prototype of modal pop-up
    Added just a basic modal and configured its functionality
    
    Modal pop-up will be used for displaying project's details

--- src/components/card.js
+++ src/components/card.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import {
   Card,
   CardTitle,
@@ -10,15 +10,54 @@ import {
 } from "react-mdl";
 
 import "./card.css";
+import Modal from "./modal";
+
+function App() {
+  const modalRef = React.useRef();
+
+  const openModal = () => {
+    modalRef.current.openModal();
+  };
+
+  return (
+    <div className="App">
+      <button onClick={openModal}>Open Modal</button>
+      <Modal ref={modalRef}>
+        <h1>Modal Header</h1>
+        <p>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+            eligendi esse facere illo in minima nulla quis reiciendis. Eligendi
+            impedit nostrum quam quod reprehenderit, ullam veritatis. Fuga
+            provident quos velit.
+          </span>
+          <span>
+            Accusantium ad, alias animi et eum, excepturi explicabo fuga iusto
+            magnam maxime minima molestias nam nemo nostrum pariatur
+            perspiciatis porro quae quibusdam quidem quis repudiandae sed ullam
+            vel, veniam vero.
+          </span>
+          <span>
+            Eligendi nulla quasi quibusdam quod saepe suscipit tenetur voluptas
+            voluptate! Accusamus amet, commodi culpa distinctio dolor eveniet
+            expedita hic iure magnam magni mollitia nulla officia quas,
+            reiciendis repellat sapiente, veniam!
+          </span>
+        </p>
+        <button onClick={() => modalRef.current.close()}>Close Modal</button>
+      </Modal>
+    </div>
+  );
+}
 
 class ProjectCard extends Component {
   render() {
     return (
-      <div>
+      <div id="card">
         <Card
-          id="card"
           shadow={5}
           style={{
+            position: "inherit",
             backgroundColor: "#171b22 ",
             borderRadius: "10px",
             minWidth: "450px",
@@ -57,7 +96,8 @@ class ProjectCard extends Component {
           </CardActions>
           <CardMenu style={{ color: "#fff" }}>
             <IconButton name="share" />
-          </CardMenu>
+          </CardMenu>{" "}
+          <App />
         </Card>
       </div>
     );
